1a0208d72513e48996645dcf7d85f68f69001d80
Kash Somani <kash@kashsomani.com>
2023-01-04 03:11:45 -0800

    vu

--- index.js
+++ index.js
@@ -1,63 +1,173 @@
-// require("dotenv").config()
-// const axios = require("axios")
-// const options = {
-//   url: "https://kash-somani-s-workspace-bet50q.us-east-1.xata.sh/db/ccc-floor:main/tables/floors/query",
-//   method: "POST",
-//   headers: {
-//     Authorization: `Bearer ${process.env.TOKEN}`,
-//     "Content-Type": "application/json",
-//   },
-//   body: '{"sort":{"date":"asc"}}',
-// }
-
-// axios(options)
-//   .then(response => response.data)
-//   .then(response => {
-//     let records = response.records.slice(0, 9)
-
-//     records = records.sort((a, b) => {
-//       if (a.rank == "SE") {
-//         return 1
-//       }
-//       if (b.rank == "SE") {
-//         return -1
-//       }
-//       return Number(b.rank) - Number(a.rank)
-//     })
-//     records.forEach(el =>
-//       console.log(`rank:${el.rank}, floor:${el.floor}, date:${el.date}`)
-//     )
-//   })
-//   .catch(err => console.error(err))
 const puppeteer = require("puppeteer")
-async function main() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-  })
-  await page.goto("https://cnft.tools/radioactivecrocsclub")
-  await page.waitForSelector(".mantine-1s9i825")
-  let maxInput = await page.evaluate(
-    () =>
-      Array.from(
-        document.querySelectorAll(".mantine-1s9i825")
-      )[1]
+// haha
+// cardanocrocsclub
+
+const getFloorFromCnftTools =
+  async function getFloorFromCnftTools(
+    collectionName,
+    range
+  ) {
+    console.log(
+      "getFloorFromCnftTools collectionName:" +
+        collectionName +
+        " range:" +
+        JSON.stringify(range)
+    )
+    try {
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.setViewport({
+        width: 1200,
+        height: 1500,
+      })
+
+      await page.goto(
+        "https://cnft.tools/" + collectionName,
+        {
+          waitUntil: "networkidle2",
+        }
+      )
+
+      await page.click("#sort-type")
+      await page.keyboard.press("ArrowDown")
+      await page.keyboard.press("Enter")
+      await page.waitForTimeout(5000)
+
+      await page.type(
+        'input[placeholder="Min"]:first-child',
+        range.min.toString()
+      )
+      await page.type(
+        'input[placeholder="Max"]:first-child',
+        range.max.toString()
+      )
+      await page.click("button.mantine-txbd7p")
+      await page.waitForTimeout(1000)
+      const html = await page.content()
+
+      const result = []
+      let idx = 0
+      let end_idx = 0
+      do {
+        idx = html.indexOf('line-height: 1.5;">', idx)
+        if (idx == -1) {
+          continue
+        }
+        idx = idx + 'line-height: 1.5;">'.length
+
+        end_idx = html.indexOf("<", idx)
+        const asset_name = html
+          .substring(idx, end_idx)
+          .replace(" # ", "")
+        idx = end_idx
+
+        idx = html.indexOf('line-height: 1.5;">', idx)
+        idx = idx + 'line-height: 1.5;">'.length
+        idx = html.indexOf(">", idx)
+        end_idx = html.indexOf("<", idx)
+        const asset_price = html.substring(idx, end_idx)
+        const trimmed_asset_price = asset_price.substring(
+          1,
+          asset_price.length - 3
+        )
+        idx = end_idx
+        console.log({
+          collectionName: asset_name,
+          price: parseInt(trimmed_asset_price),
+        })
+        result.push({
+          collectionName: asset_name,
+          price: parseInt(trimmed_asset_price),
+        })
+      } while (idx != -1)
+
+      await browser.close()
+      return result
+    } catch (error) {
+      console.log("error", error)
+    }
+
+    return []
+  }
+const getSingleFloor = async (collectionName, range) => {
+  console.log(
+    "getFloorFromCnftTools collectionName:" +
+      collectionName +
+      " range:" +
+      JSON.stringify(range)
   )
-  console.log(maxInput)
-  maxInput = maxInput[1]
-  console.log(maxInput)
-  maxInput.type("1000")
-  await page.waitForTimeout(1000)
-  await page.keyboard.press("Tab")
-  await page.keyboard.press("Enter")
-  let text = await page.evaluate(() => {
-    return document.querySelector(".mantine-5xhj36")
-      .innerHTML
-  })
-  console.log(maxInput)
-  console.log(text)
-  await browser.close()
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({
+      width: 1200,
+      height: 1500,
+    })
+
+    await page.goto(
+      "https://cnft.tools/" + collectionName,
+      {
+        waitUntil: "networkidle2",
+      }
+    )
+
+    await page.click("#sort-type")
+    await page.keyboard.press("ArrowDown")
+    await page.keyboard.press("Enter")
+    await page.waitForTimeout(5000)
+
+    await page.type(
+      'input[placeholder="Min"]:first-child',
+      range.min.toString()
+    )
+    await page.type(
+      'input[placeholder="Max"]:first-child',
+      range.max.toString()
+    )
+    await page.click("button.mantine-txbd7p")
+    await page.waitForTimeout(1000)
+    const html = await page.content()
+
+    const result = []
+    let idx = 0
+    let end_idx = 0
+    idx = html.indexOf('line-height: 1.5;">', idx)
+    if (idx == -1) {
+      return {
+        range,
+        price: "None",
+      }
+    }
+    idx = idx + 'line-height: 1.5;">'.length
+
+    end_idx = html.indexOf("<", idx)
+    const asset_name = html
+      .substring(idx, end_idx)
+      .replace(" # ", "")
+    idx = end_idx
+
+    idx = html.indexOf('line-height: 1.5;">', idx)
+    idx = idx + 'line-height: 1.5;">'.length
+    idx = html.indexOf(">", idx)
+    end_idx = html.indexOf("<", idx)
+    const asset_price = html.substring(idx, end_idx)
+    const trimmed_asset_price = asset_price.substring(
+      1,
+      asset_price.length - 3
+    )
+    idx = end_idx
+    await browser.close()
+    return {
+      range,
+      price: parseInt(trimmed_asset_price),
+    }
+  } catch (error) {
+    console.log("error", error)
+  }
+
+  return []
 }
-main()
+getSingleFloor("babycrocsclub", {
+  min: 1000,
+  max: 2000,
+}).then(r => console.log(r))
